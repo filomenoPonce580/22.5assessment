19a2c232575bcca4609eabd95902c337c3b8230f
filomenoPonce580 <110742301+filomenoPonce580@users.noreply.github.com>
2023-04-12 12:47:01 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import Content from "./Content";
+import Header from "./Header";
+
+function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [fontSize, setFontSize] = useState(12)
+  const toggleLoggedIn = () => setLoggedIn(!loggedIn);
+  const increaseFont = () => setFontSize(fontSize + 2)
+  return (
+    <div>
+      <Header loggedIn={loggedIn} handleLoggedInClick={toggleLoggedIn} handleFontSizeIncrease={increaseFont} />
+      <Content loggedIn={loggedIn} fontSize={fontSize}/>
+    </div>
+  );
+}
+
+export default App;
